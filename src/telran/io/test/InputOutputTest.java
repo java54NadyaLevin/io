7ae21210c8b9bd88629a96b4c4fd9baf6b83b9f9
Nadelele <nadele@gmail.com>
2024-07-10 16:28:21 +0300

    hw printDirectoryTree implemented

--- src/telran/io/test/InputOutputTest.java
+++ src/telran/io/test/InputOutputTest.java
@@ -12,33 +12,37 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashSet;
 import java.util.Set;
+
 class InputOutputTest {
 
 	private static final String STREAM_FILE = "stream-file";
 	private static final String HELLO = "Hello";
 	private static final String WRITER_FILE = "writer-file";
-@AfterAll
- static void tearDown() throws IOException {
-	Files.deleteIfExists(Path.of(STREAM_FILE));
-	Files.deleteIfExists(Path.of(WRITER_FILE));
-}
+
+	@AfterAll
+	static void tearDown() throws IOException {
+		Files.deleteIfExists(Path.of(STREAM_FILE));
+		Files.deleteIfExists(Path.of(WRITER_FILE));
+	}
+
 	@Test
-	    //Testing PrintStream class
-	//creates PrintStream object and connects it to file with name STREAM_FILE
+	// Testing PrintStream class
+	// creates PrintStream object and connects it to file with name STREAM_FILE
 	void printStreamTest() throws Exception {
-		//try resources block allows automatic closing after existing from the block
-		try(PrintStream printStream = new PrintStream(STREAM_FILE);) {
+		// try resources block allows automatic closing after existing from the block
+		try (PrintStream printStream = new PrintStream(STREAM_FILE);) {
 			printStream.println(HELLO);
 		}
-		
+
 		try (BufferedReader reader = new BufferedReader(new FileReader(STREAM_FILE))) {
 			assertEquals(HELLO, reader.readLine());
 			assertNull(reader.readLine());
 		}
 	}
+
 	@Test
-	     //The same test as for PrintStream but for PrintWriter
-	//The difference between PrintSTream and PrintWriter is that
+	// The same test as for PrintStream but for PrintWriter
+	// The difference between PrintSTream and PrintWriter is that
 	// PrintStream puts line immediately while PrintWriter puts line into a buffer
 	// The buffer flushing is happening after closing the stream
 	void printWriterTest() throws Exception {
@@ -50,58 +54,73 @@ class InputOutputTest {
 			assertNull(reader.readLine());
 		}
 	}
+
 	@Test
 	void pathTest() {
 		Path pathCurrent = Path.of(".");
 //		System.out.printf("%s - path \".\"\n",pathCurrent);
 //		System.out.printf("%s - normolized path \".\"\n", pathCurrent.normalize());
-		System.out.printf("%s - %s\n",
-				pathCurrent.toAbsolutePath().normalize(),
+		System.out.printf("%s - %s\n", pathCurrent.toAbsolutePath().normalize(),
 				Files.isDirectory(pathCurrent) ? "directory" : "file");
 		pathCurrent = pathCurrent.toAbsolutePath().normalize();
-		System.out.printf("count of levels is %d\n",
-				pathCurrent.getNameCount());
-		
+		System.out.printf("count of levels is %d\n", pathCurrent.getNameCount());
+
 	}
+
 	@Test
 	void printDirectoryTest() throws IOException {
-		printDirectory("..", 3);
+		printDirectory(".", 4);
 	}
-	private void printDirectory(String dirPathStr, int depth) throws IOException {
-		//TODO
-		//print directory content in the format with offset according to the level
-		//if depth == -1 all levels should be printed out
-		// <name> - <dir / file>
-		//      <name> 
-		//using FIles.walkFileTree
-		Path path = Path.of(dirPathStr).toAbsolutePath().normalize();
-		//TODO
-		Files.walkFileTree(path, new HashSet<>(), depth, new FileVisitor<Path>() {
 
+	private void printDirectory(String dirPathStr, int depth) throws IOException {
+
+		// TODO
+		// print directory content in the format with offset according to the level
+		// if depth == -1 all levels should be printed out
+		// <name> - <dir / file>
+		// <name>
+		// using FIles.walkFileTree
+		Path path = Path.of(dirPathStr).toAbsolutePath().normalize();
+		
+		// TODO
+		Files.walkFileTree(path, new HashSet<>(), depth, new FileVisitor<Path>() {
+			int offset = 0;
 			@Override
 			public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+				printWithOffset(dir);
+				offset++;
 				return FileVisitResult.CONTINUE;
+			}
+
+			  private void printWithOffset(Path path) {
+	                StringBuilder indent = new StringBuilder();
+	                for (int i = 0; i < offset; i++) {
+	                    indent.append("     "); // Adjust the spaces for indentation as needed
+	                }
+                String dirOrFile = Files.isDirectory(path) ? "directory" : "file";
+                System.out.println(indent.toString() + path.getFileName() 
+                + " - " + dirOrFile);
 			}
 
 			@Override
 			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-				// TODO Auto-generated method stub
-				return null;
+				printWithOffset(file);
+				return FileVisitResult.CONTINUE;
 			}
 
 			@Override
 			public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-				// TODO Auto-generated method stub
-				return null;
+				System.err.println(exc);
+				return FileVisitResult.CONTINUE;
 			}
 
 			@Override
 			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-				// TODO Auto-generated method stub
-				return null;
+				offset--;
+				return FileVisitResult.CONTINUE;
 			}
 		});
-		
+
 	}
 
 }
